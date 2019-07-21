fdaec50259c9287aefbb2a9a917b9f181f00502f
Ben Stucke <ben-stucke@hotmail.de>
2019-07-21 16:12:03 +0200

    initial commit

---
+++ src/main/java/pbjsystems/newyearservice/NewyearserviceApplication.java
@@ -0,0 +1,13 @@
+package pbjsystems.newyearservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyearserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewyearserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pbjsystems/newyearservice/NewyearserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package pbjsystems.newyearservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NewyearserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
